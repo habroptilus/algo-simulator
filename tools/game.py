b173ebc4927e00b9bf702f15ec6ab7aa35949adf
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2024-01-10 23:15:44 +0900

    fix

--- tools/game.py
+++ tools/game.py
@@ -6,6 +6,7 @@ from tools.card_list import Deck, Hands
 from tools.player import Player
 from tools.consts import PLAYERS_NUM
 from tools.logic import get_attack, get_attack_from_input
+from tools.attack import Attack
 
 
 def print_status(players: List[Player]) -> None:
@@ -24,8 +25,8 @@ def print_status(players: List[Player]) -> None:
 
 class Game:
     def __init__(self, colors: List[str], numbers: List[int], max_turns: int,
-                 start_attacker: int,  max_hands: int, player_id_list=List[int],
-                 human_player: Optional[int] = None):
+                 start_attacker: int,  max_hands: int, player_id_list: List[int],
+                 include_human_player: bool, human_player: Optional[int] = None, sleep_seconds: int = 3):
         self.colors = colors
         self.numbers = numbers
         self.max_turns = max_turns
@@ -33,30 +34,11 @@ class Game:
         self.human_player: Optional[int] = human_player
         self.max_hands = max_hands
         self.player_id_list = player_id_list
-
-    def init_player(self, deck: Deck, player_id: int, name: str) -> Player:
-        hands = Hands(cards=[deck.draw(player_id=player_id)
-                             for _ in range(self.max_hands)])
-        return Player(player_id=player_id, hands=hands, name=name)
-
-    def init_players(self, deck: Deck, human_player: int) -> List[Player]:
-        players: List[Player] = []
-        cpu_num = 1
-        for player_id in self.player_id_list:
-            if player_id == human_player:
-                name = "You "
-            else:
-                name = f"CPU{cpu_num}"
-                cpu_num += 1
-            player = self.init_player(
-                deck=deck, player_id=player_id, name=name)
-            players.append(player)
-        return players
+        self.include_human_player = include_human_player
+        self.sleep_seconds = sleep_seconds
 
     def start(self):
-        human_player = self.human_player if self.human_player is not None else random.choice(
-            self.player_id_list)
-        print(f"You are Player{human_player}.")
+        human_player = self.get_human_player()
         deck = Deck(colors=self.colors, numbers=self.numbers)
         players = self.init_players(deck=deck, human_player=human_player)
 
@@ -72,6 +54,7 @@ class Game:
                 player for player in players if player.player_id != attacker]
 
             new_card = deck.draw(player_id=player.player_id)
+            new_card_content: Optional[CardContent] = None
             if new_card is None:
                 print("deck is empty.")
             else:
@@ -80,20 +63,10 @@ class Game:
 
             has_succeeded = False
             while True:
-                if player.player_id == human_player:
-                    attack = get_attack_from_input(
-                        player=player, opponents=opponents, new_card=new_card_content, has_succeeded=has_succeeded)
-                else:
-                    print("Thinking...")
-                    time.sleep(3)
-                    attack = get_attack(
-                        player=player,
-                        opponents=opponents,
-                        new_card=new_card_content,
-                        opened_cards=opened_cards,
-                        has_succeeded=has_succeeded,
-                        history=history,
-                    )
+                attack = self.act(human_player=human_player, player=player, opponents=opponents,
+                                  new_card_content=new_card_content, has_succeeded=has_succeeded,
+                                  opened_cards=opened_cards, history=history)
+
                 if attack is None:
                     if not has_succeeded:
                         raise Exception(
@@ -106,7 +79,7 @@ class Game:
                 history.append(attack)
                 opponent = players[attack.attacked_to]
                 print("Judging...")
-                time.sleep(3)
+                time.sleep(self.sleep_seconds)
                 result = opponent.hands.judge(attack=attack)
                 if result:
                     print("Success!\n")
@@ -136,3 +109,51 @@ class Game:
             # switch attacker
             attacker = (attacker+1) % PLAYERS_NUM
             print("\n")
+
+    def get_human_player(self) -> Optional[int]:
+        if self.include_human_player:
+            human_player = self.human_player if self.human_player is not None else random.choice(
+                self.player_id_list)
+            print(f"You are Player{human_player}.")
+        else:
+            human_player = None
+            print("Any Human player doesn't join this game.")
+        return human_player
+
+    def act(self, human_player: int, player: Player, opponents: List[Player],
+            new_card_content: Optional[CardContent], has_succeeded: bool,
+            opened_cards: List[CardContent], history: List[Attack]) -> Optional[Attack]:
+        if (human_player is not None) and (player.player_id == human_player):
+            attack = get_attack_from_input(
+                player=player, opponents=opponents, new_card=new_card_content, has_succeeded=has_succeeded)
+        else:
+            print("Thinking...")
+            time.sleep(self.sleep_seconds)
+            attack = get_attack(
+                player=player,
+                opponents=opponents,
+                new_card=new_card_content,
+                opened_cards=opened_cards,
+                has_succeeded=has_succeeded,
+                history=history,
+            )
+        return attack
+
+    def init_player(self, deck: Deck, player_id: int, name: str) -> Player:
+        hands = Hands(cards=[deck.draw(player_id=player_id)
+                             for _ in range(self.max_hands)])
+        return Player(player_id=player_id, hands=hands, name=name)
+
+    def init_players(self, deck: Deck, human_player: Optional[int]) -> List[Player]:
+        players: List[Player] = []
+        cpu_num = 1
+        for player_id in self.player_id_list:
+            if (human_player is not None) and (player_id == human_player):
+                name = "You "
+            else:
+                name = f"CPU{cpu_num}"
+                cpu_num += 1
+            player = self.init_player(
+                deck=deck, player_id=player_id, name=name)
+            players.append(player)
+        return players
