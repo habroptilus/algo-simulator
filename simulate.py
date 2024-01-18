0647f47cfba0b4a66e4e316c8bc06a144446b40a
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2024-01-18 21:21:03 +0900

    fix

--- simulate.py
+++ simulate.py
@@ -8,29 +8,20 @@ if __name__ == '__main__':
     parser.add_argument('--trials', '-t', type=int, default=10)
 
     args = parser.parse_args()
-    winners = []
-    proba_list = []
-    attack_results = []
-    different_between_strategies_list = []
-    turns = []
-    skip_counts = []
 
-    logics = [EpsilonGreedy(epsilon=0),
-              EpsilonGreedy(epsilon=1)]
-    game = Game(logics=logics, sleep_seconds=0)
+    epsilons = [0, 0.1, 0.5, 1]
+    results = {}
 
-    for _ in range(args.trials):
-        outputs = game.start()
-        proba_list += outputs["proba_list"]
-        attack_results += outputs["attack_results"]
-        turns.append(outputs["turns"])
-        winners.append(outputs["winner"])
-        skip_counts.append(outputs["skip_count"])
+    for e1 in epsilons:
+        for e2 in epsilons:
+            logics = [EpsilonGreedy(epsilon=e1),
+                      EpsilonGreedy(epsilon=e2)]
+            game = Game(logics=logics, sleep_seconds=0)
+            winners = []
+            for _ in range(args.trials):
+                outputs = game.start()
+                winners.append(outputs["winner"])
 
-    target_player = logics[0].name
-    print(
-        f"Win rate of {target_player}: {(len(winners)-sum(winners))/len(winners)}")
-    assert len(proba_list) == len(attack_results)
-    print(f"Calib: {sum(proba_list)/sum(attack_results)}")
-    print(f"Avg turns: {sum(turns)/len(turns)}")
-    print(skip_counts)
+            results[f"{e1}_{e2}"] = (len(winners)-sum(winners))/len(winners)
+
+    print(results)
